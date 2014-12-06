14a9e8ca3fda92e6a7f04a587c4d3ab27ab3e2c9
tmschl <timothy.schiller@gmail.com>
2014-12-05 17:43:33 -0800

    more metal smittenness

--- build.js
+++ build.js
@@ -4,13 +4,24 @@ var Metalsmith  = require('metalsmith'),
     templates   = require('metalsmith-templates'),
     collections = require('metalsmith-collections'),
     serve       = require('metalsmith-serve'),
+    watch       = require('metalsmith-watch'),
     autoprefixer = require('metalsmith-autoprefixer');
 
+Metalsmith(__dirname)
+    .source('src/public/scss')
+    .destination('build/public')
+    .use(sass({
+      outputStyle: 'expanded',
+      outputDir: 'css'
+    }))
+    .use(autoprefixer())
+    .use(watch('**/*.scss'))
+    .build(function(err) {
+      if (err) throw err;
+    })
 
 Metalsmith(__dirname)
     .use(markdown())
-    .use(sass())
-    .use(autoprefixer())
     .use(collections({
       articles: {
         sortBy: 'date',
